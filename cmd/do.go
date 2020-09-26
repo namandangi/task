c82750ba470c37d26a3744741d8468fff3aee51e
namandangi <danginaman55@gmail.com>
2020-09-26 12:03:38 +0530

    link do functionality with db

--- cmd/do.go
+++ cmd/do.go
@@ -2,8 +2,10 @@ package cmd
 
 import(
   "fmt"
+  "os"
   "strconv"
   "github.com/spf13/cobra"
+  "github.com/task/db"
   )
 
 var doCmd = &cobra.Command{
@@ -19,7 +21,24 @@ var doCmd = &cobra.Command{
         fmt.Println("Failed to parse the argument :",arg)
       }
     }
-    fmt.Println(ids)
+    tasks, err := db.AllTasks()
+    if err != nil {
+      fmt.Println("Something went wrong",err)
+      os.Exit(1)
+    }
+    for _, id := range ids {
+      if id <= 0 || id > len(tasks) {
+        fmt.Println("Invalid task id",id)
+        continue
+      }
+      task := tasks[id-1]
+      err := db.DeleteTask(task.Key)
+      if err != nil {
+        fmt.Printf("Failed to mark Task %d as completed. Error: %s\n" ,id,err)
+      } else {
+        fmt.Printf("Marked Task %d as completed.\n",id)
+      }
+    }
   },
 }
 
